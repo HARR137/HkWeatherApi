aafd4be71d981429e28e1844e4788ddf657f7b9c
tonylo2811 <tonylo19961128@gmail.com>
2023-04-09 03:10:40 +0800

    update: object mapping update

---
+++ key.js
@@ -0,0 +1,213 @@
+const stationObject = {
+  zh: {
+    hka: "赤鱲角",
+    cch: "長洲",
+    clearwaterbay: "清水灣",
+    hpv: "跑馬地",
+    hko: "天文台",
+    hkp: "香港公園",
+    se1: "啟德跑道公園",
+    kausaichau: "滘西洲",
+    kp: "京士柏",
+    klt: "九龍城",
+    ktg: "觀塘",
+    lfs: "流浮山",
+    ngongping: "昂坪",
+    paktamchung: "北潭涌",
+    pengchau: "坪洲",
+    skg: "西貢",
+    sha: "沙田",
+    ssp: "深水埗",
+    skw: "筲箕灣",
+    sek: "石崗",
+    sheungshui: "上水",
+    sty: "赤柱",
+    tkl: "打鼓嶺",
+    tailoong: "大隴",
+    taimeituk: "大美督",
+    taimoshan: "大帽山",
+    tpo: "大埔",
+    tatescarin: "大老山",
+    thepeak: "山頂",
+    jkb: "將軍澳",
+    ty1: "青衣",
+    twn: "荃灣可觀",
+    tw: "荃灣城門谷",
+    tun: "屯門",
+    waglanisland: "橫瀾島",
+    hkwetlandpark: "濕地公園",
+    hks: "黃竹坑",
+    wts: "黃大仙",
+    yuenlongpark: "元朗公園",
+  },
+  en: {
+    hka: "Chek Lap Kok",
+    cch: "Cheung Chau",
+    clearwaterbay: "Clear Water Bay",
+    hpv: "Happy Valley",
+    hko: "Hong Kong Observatory",
+    hkp: "Hong Kong Park",
+    se1: "Kai Tak Runway Park",
+    kausaichau: "Kau Sai Chau",
+    kp: "King's Park",
+    klt: "Kowloon City",
+    ktg: "Kwun Tong",
+    lfs: "Lau Fau Shan",
+    ngongping: "Ngong Ping",
+    paktamchung: "Pak Tam Chung",
+    pengchau: "Peng Chau",
+    skg: "Sai Kung",
+    sha: "Sha Tin",
+    ssp: "Sham Shui Po",
+    skw: "Shau Kei Wan",
+    sek: "Shek Kong",
+    sheungshui: "Sheung Shui",
+    sty: "Stanley",
+    tkl: "Ta Kwu Ling",
+    tailoong: "Tai Loong",
+    taimeituk: "Tai Mei Tuk",
+    taimoshan: "Tai Mo Shan",
+    tpo: "Tai Po",
+    tatescarin: "Tate's Cairn",
+    thepeak: "The Peak",
+    jkb: "Tseung Kwan O",
+    ty1: "Tsing Yi",
+    twn: "Tsuen Wan Ho Koon",
+    tw: "Tsuen Wan Shing Mun Valley",
+    tun: "Tuen Mun",
+    waglanisland: "Waglan Island",
+    hkwetlandpark: "Hong Kong Wetland Park",
+    hks: "Wong Chuk Hang",
+    wts: "Wong Tai Sin",
+    yuenlongpark: "Yuen Long Park",
+  },
+  district: {
+    kowloonCity: {
+      name: "Kowloon City",
+      position: {
+        klt: "九龍城",
+        se1: "啟德跑道公園",
+      },
+    },
+    taiPo: {
+      name: "Tai Po",
+      position: {
+        taimeituk: "大美督",
+        tpo: "大埔",
+      },
+    },
+    centralAndWestern: {
+      name: "Central and Western District",
+      position: {
+        thepeak: "山頂",
+        hkp: "香港公園",
+      },
+    },
+    yuenLong: {
+      name: "Yuen Long",
+      position: {
+        yuenlongpark: "元朗公園",
+        sek: "石崗",
+        lfs: "流浮山",
+        hkwetlandpark: "濕地公園",
+      },
+    },
+    tuenMun: {
+      name: "Tuen Mun",
+      position: {
+        tun: "屯門",
+      },
+    },
+    north: {
+      name: "North District",
+      position: {
+        sheungshui: "上水",
+        tkl: "打鼓嶺",
+      },
+    },
+    saiKung: {
+      name: "Sai Kung",
+      position: {
+        paktamchung: "北潭涌",
+        skg: "西貢",
+        jkb: "將軍澳",
+        kausaichau: "滘西洲",
+      },
+    },
+    shaTin: {
+      name: "Sha Tin",
+      position: {
+        sha: "沙田",
+      },
+    },
+    eastern: {
+      name: "Eastern District",
+      position: {
+        skw: "筲箕灣",
+      },
+    },
+    yauTsimMong: {
+      name: "Yau Tsim Mong",
+      position: {
+        hko: "天文台",
+        kp: "京士柏",
+      },
+    },
+    southern: {
+      name: "Southern District",
+      position: {
+        sty: "赤柱",
+        hks: "黃竹坑",
+      },
+    },
+    tsuenWan: {
+      name: "Tsuen Wan",
+      position: {
+        twn: "荃灣可觀",
+        tw: "荃灣城門谷",
+      },
+    },
+    shamShuiPo: {
+      name: "Sham Shui Po",
+      position: {
+        ssp: "深水埗",
+      },
+    },
+    wongTaiSin: {
+      name: "Wong Tai Sin",
+      position: {
+        wts: "黃大仙",
+      },
+    },
+    kwaiTsing: {
+      name: "Kwai Tsing",
+      position: {
+        ty1: "青衣",
+      },
+    },
+    outlyingIslands: {
+      name: "Outlying Islands",
+      position: {
+        hka: "赤鱲角",
+        pengchau: "坪洲",
+        ngongping: "昂坪",
+        cch: "長洲",
+        waglanisland: "橫瀾島",
+      },
+    },
+    wanChai: {
+      name: "Wan Chai",
+      position: {
+        hpv: "跑馬地",
+      },
+    },
+    kwunTong: {
+      name: "Kwun Tong",
+      position: {
+        ktg: "觀塘",
+      },
+    },
+  },
+};
+
+module.exports = stationObject;
